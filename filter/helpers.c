55ec4fba8ec11bd07fea73253a7aa6b08893a675
Iliaromanov <iliaromanov033@gmail.com>
2020-08-29 18:37:07 +0000

    working on grayscale function

--- filter/helpers.c
+++ filter/helpers.c
@@ -3,6 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            RGBTRIPLE *pixel = &image[row][col];
+            
+            int avg = (pixel->rgbtBlue + pixel->rgbtGreen + pixel->rgbtRed) / 3;
+            
+            
+        }
+    }
     return;
 }
 
