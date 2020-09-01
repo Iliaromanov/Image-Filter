4d473014ab8619c0c48d476a8d744ef13482d290
Iliaromanov <iliaromanov033@gmail.com>
2020-09-01 21:35:10 +0000

    fixed blur filter bug

--- helpers.c
+++ helpers.c
@@ -81,7 +81,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     int sumGreen;
     float total;
     
-    RGBTRIPLE copy[width][height];
+    RGBTRIPLE copy[height][width];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
